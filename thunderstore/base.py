112e2bc190d7872ee2d4c020a8b5572f75ad48b3
Zash <zash@fantasymail.de>
2025-10-18 21:33:03 +0200

    v0.3.0

--- thunderstore/base.py
+++ thunderstore/base.py
@@ -22,7 +22,7 @@ class ThunderstoreBasePlugin(mobase.IPlugin):
         return self.base_name
 
     def version(self: mobase.IPlugin) -> mobase.VersionInfo:
-        return mobase.VersionInfo(0, 2, 1)
+        return mobase.VersionInfo(0, 3, 0)
 
     def author(self: mobase.IPlugin) -> str:
         return "Zash"
