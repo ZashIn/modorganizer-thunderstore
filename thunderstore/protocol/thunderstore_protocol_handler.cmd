e44fe619522b1f9a43130dbac9a6f2f8f1a0d8dc
Zash <zash@fantasymail.de>
2025-10-18 21:30:11 +0200

    Add .python-version to build task, with error message on mismatch MO python dll version

--- thunderstore/protocol/thunderstore_protocol_handler.cmd
+++ thunderstore/protocol/thunderstore_protocol_handler.cmd
@@ -1,3 +1,13 @@
-@echo off & setlocal
+@echo off & setlocal EnableDelayedExpansion
 set "PATH=%~dp0..\..\plugin_python\dlls;%PATH%"
 "%~dp0thunderstore_protocol_handler.exe" %*
+if %errorlevel% LSS 0 (
+    title %~dp0thunderstore_protocol_handler.exe
+    echo MO Python version mismatch^! 1>&2
+    set /p version=<"%~dp0.python-version"
+    echo Required: !version! 1>&2
+    for %%p in ("%~dp0..\..\plugin_python\dlls") do SET "dllPath=%%~fp"
+    echo Available in !dllPath!: 1>&2
+    dir /b "!dllPath!\python*" 1>&2
+    pause
+)
